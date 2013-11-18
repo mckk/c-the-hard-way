e87e45e3fa6f5e874d93cfcfc60a85f5c91bd7e6
Maciek Albin <maciek@lbin.me>
2013-11-18 18:16:40 +0100

    Don't want to install this globally

--- devpkg/db.h
+++ devpkg/db.h
@@ -1,8 +1,8 @@
 #ifndef _db_h
 #define _db_h
 
-#define DB_FILE "/usr/local/.devpkg/db"
-#define DB_DIR "/usr/local/.devpkg"
+#define DB_FILE "~/.devpkg/db"
+#define DB_DIR "~/.devpkg"
 
 int DB_init();
 int DB_list();
