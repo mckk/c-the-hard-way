ed96da2b0485ebd88ea6eca6ce3aa4946cadae58
Maciek Albin <maciek@lbin.me>
2013-11-22 17:22:17 +0100

    Fixing makefile and tests for the library

--- libcthw/tests/runtests.sh
+++ libcthw/tests/runtests.sh
@@ -1,14 +1,14 @@
 echo "Running unit tests:"
 
-for i in tests/*_test
+for i in tests/*_tests
 do
   if test -f $i
   then
-    if ./$i 2>> tests/tests.log
+    if $VALGRIND ./$i 2>> tests/tests.log
     then
       echo $i PASS
     else
-      echo "ERROR in test $i: here's tests/tests.log"
+      echo "ERROR in test $i: here is tests/tests.log"
       echo "------"
       tail tests/tests.log
       exit 1
